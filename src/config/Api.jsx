d0b1584810af3a4dae59004abce1c6acfc8fdbcf
Rachel G. <rachel.galdino@outlook.com>
2024-10-13 01:09:56 -0300

    Update API base URL to localhost

--- src/config/Api.jsx
+++ src/config/Api.jsx
@@ -3,8 +3,8 @@ import Swal from 'sweetalert2';
 import Messages from '../components/Messages';
 
 const Api = axios.create({
-    // baseURL: 'http://localhost:3000/'
-    baseURL: 'https://fs29a-backend-5aa5.onrender.com/'
+    baseURL: 'http://localhost:3000/'
+    // baseURL: 'https://fs29a-backend-5aa5.onrender.com/'
 })
 
 Api.interceptors.response.use((response) => {
